db78a573fea881daffede8300042a8c4c67613c0
Alperen Sarışan <88689564+althebeast@users.noreply.github.com>
2024-09-07 15:38:58 +0300

    Initial Commit

---
+++ GameNews/GameNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameNewsApp.swift
+//  GameNews
+//
+//  Created by Alperen Sarışan on 7.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
